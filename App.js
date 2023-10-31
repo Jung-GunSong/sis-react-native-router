6857432f974d84f492eeb62c6fba7578161c4ce1
Jung-GunSong <jsong781@gmail.com>
2023-10-30 20:48:29 -0400

    Started working log in page
    Started to get familiar with using react-native, and react-native-router.
    Have started to create a log-in page with input forms.

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NativeRouter} from "react-router-native";
+import RouteList from './RouteList';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <NativeRouter>
+        <RouteList />
+      </NativeRouter>
     </View>
+
   );
 }
 
