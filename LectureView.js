61ddead704da7214f9a422d360a87a0d9f039e46
Jung-GunSong <jsong781@gmail.com>
2023-11-01 15:47:45 -0400

    Completed api fetching logic and card renders
    Completed the code utilizes our api helper function to eventually get
    detailed lecture data for each lecture in a cohort. We also can
    dynamically render LectureCard components for each lecture.

--- LectureView.js
+++ LectureView.js
@@ -1,21 +1,38 @@
 import { useEffect, useState } from "react";
 import { Text, SafeAreaView, StyleSheet } from "react-native";
+import SisApi from "./api";
+import LectureCard from "./LectureCard";
 
 /** LectureView: See all lecture titles and schedule.
  */
 
 function LectureView() {
-  const [isLoading, setIsloading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
   const [lectureData, setLectureData] = useState();
 
   useEffect(function fetchLecturesWhenMounted() {
     async function fetchLectures() {
       // await lectures
+      // fetch from api/lecturessessions
+      // want array from results key
+      // want to dynamically generate lecture views for each object
+      //run map where we run api calls on each id
+      // use promise.all() to get new objects for each leacture
+      // set lectureData to this result
+      // map lecature Data to create LectureCard components
+      const initialLectureData = await SisApi.getLectures()
+      // console.log(`initial lecture data is`, initialLectureData);
+      const promiseArray = [];
+      initialLectureData.map(l => promiseArray.push(SisApi.getLecture(l.id)));
+      const lectureDetailedData = await Promise.all(promiseArray);
+      // console.log(`our detailed lecture data is`, lectureDetailedData);
+      setLectureData(lectureDetailedData);
+      setIsLoading(false);
     }
 
     fetchLectures();
-    setIsloading(false);
-  });
+
+  }, []);
 
   if (isLoading === true) {
     return (
@@ -25,9 +42,17 @@ function LectureView() {
     )
   }
 
+  console.log(`lectureData is `, lectureData);
   return (
     <SafeAreaView style={styles.container}>
       <Text>LectureView</Text>
+      {lectureData &&
+      lectureData.map(l => (<LectureCard
+      key={l.id}
+      title={l.title}
+      description={l.description}
+      startAt={l.start_at}
+      staff={l.staff} />))}
     </SafeAreaView>
   );
 }
